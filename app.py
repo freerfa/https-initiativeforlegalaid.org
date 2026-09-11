68949b1fb216f06a77c6dcf284c72b1df8d17df2
Free Mirghani <freemirghani@Frees-MacBook-Pro.local>
2026-09-11 14:36:13 +0200

    Improve site UI and deployment config

--- app.py
+++ app.py
@@ -196,4 +196,5 @@ def update_site():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", "5000"))
+    app.run(debug=True, host="0.0.0.0", port=port)
